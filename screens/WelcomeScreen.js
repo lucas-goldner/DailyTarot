944f66acf2a01d9f2178b6b21dbc61c5728e144a
Lucas Goldner <lucas.goldner@googlemail.com>
2020-11-03 23:51:13 +0100

    Welcome is Done

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,18 +1,51 @@
 import React from "react";
-import { ImageBackground, StyleSheet } from "react-native";
+import { Image, ImageBackground, StyleSheet, View, Text } from "react-native";
 
 function WelcomeScreen(props) {
   return (
     <ImageBackground
       style={styles.background}
       source={{ uri: "https://picsum.photos/1920/1080" }}
-    ></ImageBackground>
+    >
+      <View style={styles.logoContainer}>
+        <Image source={require("../assets/favicon.png")} style={styles.logo} />
+        <Text style={styles.title}>Daily Tarot</Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  title: {
+    color: "#4ecdc4",
+    backgroundColor: "#fc5c65",
+    fontSize: 50,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
   },
 });
 
