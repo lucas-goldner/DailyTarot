ddc2cfa62371bc1bcf32b162cf954f0896297da2
Lucas Goldner <lucas.goldner@googlemail.com>
2020-11-04 21:01:56 +0100

    Basic Bottombar is done

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import * as React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Image,
+} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
@@ -38,7 +44,6 @@ function MyTabs() {
       shifting={true}
       activeColor={colours.bg}
       labelStyle={{ fontSize: 12 }}
-      style={{ backgroundColor: "tomato" }}
     >
       <Tab.Screen
         name="Feed"
@@ -48,17 +53,6 @@ function MyTabs() {
           tabBarColor: "tomato",
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="home" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Notifications"
-        component={Notifications}
-        options={{
-          tabBarLabel: "Updates",
-          tabBarColor: colours.secondary,
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="bell" color={color} size={26} />
           ),
         }}
       />
@@ -79,11 +73,59 @@ function MyTabs() {
 }
 
 export default function App() {
+  const toggleOpen = () => {};
   return (
     <NavigationContainer>
       <MyTabs />
+      <View style={styles.btnCircle}>
+        <TouchableWithoutFeedback onPress={() => toggleOpen()}>
+          <View style={[styles.button, styles.actionBtn]}>
+            <Image
+              style={{ width: 60, height: 60 }}
+              resizeMode="contain"
+              source={require("./assets/plusIcon.png")}
+            />
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  button: {
+    width: 60,
+    height: 60,
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "grey",
+    shadowOpacity: 0.1,
+    shadowOffset: { x: 2, y: 0 },
+    shadowRadius: 2,
+    borderRadius: 30,
+    position: "absolute",
+    bottom: 20,
+    right: 0,
+    top: 5,
+    left: 5,
+    shadowOpacity: 5.0,
+  },
+  actionBtn: {
+    backgroundColor: "#1E90FF",
+    backgroundColor: colours.primaryThick,
+    textShadowOffset: { width: 5, height: 5 },
+    textShadowRadius: 10,
+    borderWidth: 2,
+    borderColor: "#fff",
+  },
+  btnCircle: {
+    position: "absolute",
+    alignSelf: "center",
+    backgroundColor: colours.bg,
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    bottom: 10,
+    zIndex: 10,
+  },
+});
